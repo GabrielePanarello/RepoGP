80ba3eb37a7203b6554288dc0b4ebac9f985a384
GabrielePanarello <gbrlpanarello@gmail.com>
2018-01-29 15:38:52 +0100

    lezione2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Work';
+  items: string[] = [];
+
+  constructor(){
+    this.items.push("pippo");
+    this.items.push("pippuzzo");
+    this.items.push("dino");
+    this.items.push("gino");
+  }
 }
